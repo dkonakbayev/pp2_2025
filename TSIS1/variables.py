d106ff829e184c2b3007a9f01035b4f2448317aa
Daulet <konakbaevdaulet2001@gmail.com>
2025-01-19 17:11:43 +0500

    tsis1 commit

--- TSIS1/variables.py
+++ TSIS1/variables.py
@@ -38,84 +38,84 @@ print(y)
 
 
 
-myvar = "John"
-my_var = "John"
-_my_var = "John"
-myVar = "John"
-MYVAR = "John"
-myvar2 = "John"
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
 
 
 
 
-x, y, z = "Orange", "Banana", "Cherry"
-print(x)
-print(y)
-print(z)
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
 
 
 
 
-x = y = z = "Orange"
-print(x)
-print(y)
-print(z)
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
 
 
 
-fruits = ["apple", "banana", "cherry"]
-x, y, z = fruits
-print(x)
-print(y)
-print(z)
-
-
-
-
-x = "Python"
-y = "is"
-z = "awesome"
-print(x, y, z)
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
 
 
 
 
-
-x = "Python "
-y = "is "
-z = "awesome"
-print(x + y + z)
-
-
-
-x = 5
-y = 10
-print(x + y)
-
-
-
-x = 5
-y = "John"
-print(x, y)
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
 
 
 
 
 
-x = "awesome"
-def myfunc():
-  print("Python is " + x)
-myfunc()
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
 
 
 
-def myfunc():
-  global x
-  x = "fantastic"
+# x = 5
+# y = 10
+# print(x + y)
 
-myfunc()
 
-print("Python is " + x)
+
+# x = 5
+# y = "John"
+# print(x, y)
+
+
+
+
+
+# x = "awesome"
+# def myfunc():
+#   print("Python is " + x)
+# myfunc()
+
+
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+
+# myfunc()
+
+# print("Python is " + x)
 
 
 
